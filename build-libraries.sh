7ac44f20d0592346cc2aa03953b62aa9e993d5ec
coenicorn.blauwzee@gmail.com <coenicorn.blauwzee@gmail.com>
2023-03-17 20:11:58 +0100

    fixed broken library build

--- build-libraries.sh
+++ build-libraries.sh
@@ -10,7 +10,7 @@ sudo apt install build-essential git -y
 # raylib library dependencies
 sudo apt install libasound2-dev mesa-common-dev libx11-dev libxrandr-dev libxi-dev xorg-dev libgl1-mesa-dev libglu1-mesa-dev -y
 
-if ! ([[ -f "$BASEDIR/external/raylib" ]])
+if ! ([[ -d "$BASEDIR/external/raylib" ]])
 then
     echo "Raylib not present; Try cloning with --resurse-submodules"
 
