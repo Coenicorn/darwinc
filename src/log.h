94d8c2998d18586151d61b9f021049e0a07edd9e
Coenicorn <coenicorn.blauwzee@gmail.com>
2023-03-25 10:52:54 +0100

    code updates

--- src/log.h
+++ src/log.h
@@ -1,8 +1,10 @@
 #ifndef DC_LOG_H
 #define DC_LOG_H
 
+#include <stdio.h>
+
 /* Logs a message */
-void DC_Log(const char *message)
+static inline void DC_Log(const char *message)
 {
     printf("DarwinC: %s\n", message);
 }
--- src/main.c
+++ src/main.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 
 #include "world.h"
+#include "ecs.h"
+
+#include "dyve.h"
 
 int main(void)
 {
 
-    DC_World *g_world = DC_LoadWorldFromFile("empty.wld");;
+    // DC_World *g_world = DC_LoadWorldFromFile("empty_16.wld");
 
-    DC_FreeWorld(g_world);
+    // DC_FreeWorld(g_world);
+
+    ECS_Container *g_ECS = ECS_CreateECS();
+
+    ECS_Destroy(g_ECS);
 
     return 0;
 }
