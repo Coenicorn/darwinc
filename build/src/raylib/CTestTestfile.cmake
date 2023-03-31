ffa533076954868d984d9de2700d4ce6ec6a176c
Coenicorn <coenicorn.blauwzee@gmail.com>
2023-03-31 14:03:56 +0200

    cleaned repo

--- build/src/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/coenicorn-mint/dev/projects-internal/darwinc/src/raylib
-# Build directory: /home/coenicorn-mint/dev/projects-internal/darwinc/build/src/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
